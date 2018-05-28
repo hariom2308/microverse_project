da6c3e35fc06dd7513488818fa0e72692b5c10b1
ewamal <maluseckae@gmail.com>
2018-05-28 14:00:00 +0200

    finished milesone 1

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => res.send('Hello World wffew!'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
