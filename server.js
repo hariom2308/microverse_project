29a340b61e535aa86a844dbca12b193cde47a1d2
hariom2308 <39335190+hariom2308@users.noreply.github.com>
2018-05-28 14:01:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World wffew!'))
+app.get('/', (req, res) => res.send('Hello from Poland & India!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
