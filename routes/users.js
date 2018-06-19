ac89a48d78e6faa4b65320013c08fd80198dfaa1
ewamal <maluseckae@gmail.com>
2018-06-19 15:44:54 +0200

    added password hashing

--- routes/users.js
+++ routes/users.js
@@ -3,46 +3,35 @@ const router = express.Router();
 const server = require('../server');
 let mongoose = require('mongoose');
 const User = require('../models/user');
+let bcrypt = require('bcrypt');
 
 router.post('/', (req,res) => {
-  const user = new User ({
-    userName: req.body.userName,
-    password: req.body.password,
-    email: req.body.email,
-    fullName: req.body.fullName
-  })
-  user
-  .save()
-  .then(result => {
-    // console.log(result);
-    res.send(result);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error: err
+    bcrypt.hash(req.body.email, 10, (err,hash) => {
+      if(err) {
+        return res.status(500).json({
+          error: err
+        });
+      } else {
+        const user = new User ({
+          userName: req.body.userName,
+          email: req.body.email,
+          password: hash,
+          fullName: req.body.fullName
+        });
+        user
+        .save()
+        .then(result => {
+          res.status(201).json({
+            message: 'user created',
+          });
+          console.log(result);
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+      }
     })
-  });
-}); //end of post
-
-router.get('/', function(req, res) {
-  User.find()
-  .exec()
-  .then(docs => {
-    //console.log(docs);
-    if(docs.length >= 0){
-      res.status(200).json(docs);
-    } else {
-      res.json({
-        message: 'No users found'
-      });
-    }
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error: err
-    });
-  });
-});  //end of get
+});
 module.exports = router;
