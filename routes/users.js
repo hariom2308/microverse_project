6da1eb0297711e701a1ab57eb44e5a5cfd6f334b
ewamal <maluseckae@gmail.com>
2018-06-19 16:01:54 +0200

    added checking if email exists first before creating new user

--- routes/users.js
+++ routes/users.js
@@ -6,32 +6,61 @@ const User = require('../models/user');
 let bcrypt = require('bcrypt');
 
 router.post('/', (req,res) => {
-    bcrypt.hash(req.body.email, 10, (err,hash) => {
-      if(err) {
-        return res.status(500).json({
-          error: err
-        });
-      } else {
-        const user = new User ({
-          userName: req.body.userName,
-          email: req.body.email,
-          password: hash,
-          fullName: req.body.fullName
-        });
-        user
-        .save()
-        .then(result => {
-          res.status(201).json({
-            message: 'user created',
-          });
-          console.log(result);
-        })
-        .catch(err => {
-          res.status(500).json({
+  User.find({email: req.body.email})
+  .exec()
+  .then(user => {
+    if (user.length >= 1) {
+      return res.status(409).json({
+        message: "email already exists"
+      });
+    } else {
+      bcrypt.hash(req.body.email, 10, (err,hash) => {
+        if(err) {
+          return res.status(500).json({
             error: err
           });
-        });
-      }
-    })
+        } else {
+          const user = new User ({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: hash,
+            fullName: req.body.fullName
+          });
+          user
+          .save()
+          .then(result => {
+            res.status(201).json({
+              message: 'user created',
+            });
+          })
+          .catch(err => {
+            res.status(500).json({
+              error: err
+            });
+          });
+        }
+      })
+    }
+  })
 });
+
+router.get('/', (req,res) => {
+  User.find()
+  .exec()
+  .then(docs => {
+    if(docs.length >= 0){
+      res.status(200).json(docs);
+    } else {
+      res.json({
+        message: 'No users found'
+      });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+})
 module.exports = router;
